7f16a47e7735f0ae89e4d075b3bd97f82133ccf6
Nickel Yadav <nickelyadav57@gmail.com>
2025-09-16 13:13:52 +0530

    :construction: wip: Logical bug needs to be fixed to yield the right number

--- nth_fibonacci/src/main.rs
+++ nth_fibonacci/src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // The nth fibonacci number is a number that follows this function: 
+    // F(n) = F(n-1) + F(n-2), with F(0) = 0, F(1) = 1
+    // Step 1: Take user input, parse it into a numerical value, handle the arms of error
+    println!("Welcome to the nth_fibonacci finder!");
+    println!("Please enter a numerical value for n to find the nth fibonacci");
+    let mut user_input = String::new();
+    io::stdin().read_line(&mut user_input).expect("Failed to read line");
+    let user_input: u32 = user_input.trim().parse().expect("Please type a number");
+
+    // Step 2: Apply the formula to the input, return the answer
+    let mut count = 1;
+    let mut result = 1;
+    loop {
+        result += user_input - count;
+        println!("Result : {result}");
+        count += 1;
+        if count == user_input {
+            println!("The {user_input}'th fibonacci number is: {result}");
+            break
+        }
+    }
+}
+
+fn calculate_fibonacci(n) {
+    // FIXME: Update logic and test results
 }
